509fd3be2e12bbb276174552108f9202cd08acce
svnmurali-2004 <123376873+svnmurali-2004@users.noreply.github.com>
2024-02-02 08:44:28 +0530

    ninth commit

---
+++ adds.js
@@ -0,0 +1,29 @@
+(function(avinob){
+    var d = document,
+        s = d.createElement('script'),
+        l = d.scripts[d.scripts.length - 1];
+    s.settings = avinob || {};
+    s.src = "\/\/descriptive-monitor.com\/bwXyV\/sLd.G\/lu0YYsWOdcisYnWm5\/u-ZNXIIB\/aebmE9AufZdUplvkkPwT-UGxNNNzGQ\/0\/O\/DSAetINjT\/E\/3rNsDvQJ4bM\/QV";
+    s.async = true;
+    l.parentNode.insertBefore(s, l);
+    })({})
+
+    (function(icyi){
+        var d = document,
+            s = d.createElement('script'),
+            l = d.scripts[d.scripts.length - 1];
+        s.settings = icyi || {};
+        s.src = "\/\/descriptive-monitor.com\/a\/Ws5.wKYdWxdbl\/QO2a9fkaZ\/T\/9s6\/bC2\/5MlISbWlQB9sNBT-EH3gNPDzQY1\/OyCV0\/1\/MsTbce0oNZD\/UM5W";
+        s.async = true;
+        l.parentNode.insertBefore(s, l);
+        })({})
+
+        (function(wcth){
+            var d = document,
+                s = d.createElement('script'),
+                l = d.scripts[d.scripts.length - 1];
+            s.settings = wcth || {};
+            s.src = "\/\/descriptive-monitor.com\/blXpVrs.dRGHlZ0bYeWAdhi\/YJWp5\/uPZAX\/Iy\/Ze\/mQ9tuBZWU\/lmkNPHT\/U\/xYNXz-Uzy\/MITIg\/tlN\/TeEL3DNdTbIQxFO\/QK";
+            s.async = true;
+            l.parentNode.insertBefore(s, l);
+            })({})
